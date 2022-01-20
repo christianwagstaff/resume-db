15d2a4b236f237625154ed7a8b63bd06580b86da
christianwagstaff <=>
2022-01-20 12:29:58 -0800

    changed CORS middleware

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const userRoute = require("./routes/users");
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 // Initialize MongoDB Database
 require("./config/mongoDB_production");
@@ -28,7 +29,6 @@ require("./config/passport")(passport);
 app.use(passport.initialize());
 
 // Use Global Middleware
-app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
